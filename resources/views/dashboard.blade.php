ec54b8d0ec87f1bdbb6482a197fbc0610c0e7fad
Hanifi Abrar Setiawan <hanifisetiawan@gmail.com>
2026-01-01 11:34:16 +0800

    minor bahasa change

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -108,7 +108,7 @@
                                     </a>
                                 </div>
                             @else
-                                <p class="text-gray-800 dark:text-gray-700">You haven't registered any products yet.</p>
+                                <p class="text-gray-800 dark:text-gray-700">YKamu belum mempunyai produk.</p>
                                 <a href="{{ route('products.create') }}"
                                     class="inline-flex items-center px-4 py-2 mt-4 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
                                     Tambah Produk pertama
@@ -131,16 +131,16 @@
                                                 <tr>
                                                     <th
                                                         class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">
-                                                        Name</th>
+                                                        Nama</th>
                                                     <th
                                                         class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">
                                                         Email</th>
                                                     <th
                                                         class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">
-                                                        UMKM Name</th>
+                                                        Nama UMKM</th>
                                                     <th
                                                         class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">
-                                                        City</th>
+                                                        Kota</th>
                                                     <th
                                                         class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-300 uppercase tracking-wider">
                                                         Actions</th>
@@ -179,11 +179,11 @@
                                     <div class="mt-4">
                                         <a href="{{ route('pembina.binaan') }}"
                                             class="text-indigo-600 hover:text-indigo-900 dark:text-indigo-400 dark:hover:text-indigo-300">
-                                            View All Binaan &rarr;
+                                            Lihat Semua Binaan &rarr;
                                         </a>
                                     </div>
                                 @else
-                                    <p class="text-gray-800 dark:text-gray-200">You don't have any binaan yet.</p>
+                                    <p class="text-gray-800 dark:text-gray-200">Anda belum mempunyai Binaan.</p>
                                 @endif
                             </div>
                         @endif
