299f0528d28caa2991f69c01498c3e0d057c9232
Hanifi Abrar Setiawan <hanifisetiawan@gmail.com>
2026-01-01 11:40:08 +0800

    update minor spelling  mistake

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -108,7 +108,7 @@
                                     </a>
                                 </div>
                             @else
-                                <p class="text-gray-800 dark:text-gray-700">YKamu belum mempunyai produk.</p>
+                                <p class="text-gray-800 dark:text-gray-700">Anda belum mempunyai produk.</p>
                                 <a href="{{ route('products.create') }}"
                                     class="inline-flex items-center px-4 py-2 mt-4 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
                                     Tambah Produk pertama
